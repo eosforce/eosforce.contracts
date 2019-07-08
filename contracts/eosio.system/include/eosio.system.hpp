b82efac20850f900792120d3992381648241227b
fanyang <fanyang.coder@gmail.com>
2019-07-08 16:56:53 +0800

    add transfer to eosio.system

--- contracts/eosio.system/include/eosio.system.hpp
+++ contracts/eosio.system/include/eosio.system.hpp
@@ -1,7 +1,3 @@
-/**
- *  @file
- *  @copyright defined in eos/LICENSE.txt
- */
 #pragma once
 
 #include <eosio/asset.hpp>
@@ -11,20 +7,36 @@
 
 namespace eosio {
 
-   using std::string;
+using std::string;
 
-   /**
-    * @defgroup system_contract eosio.system
-    * @ingroup eosiocontracts
-    *
-    * eosio.system contract
-    *
-    * @details eosio.system is the system contract for eosforce
-    * @{
-    */
-   class [[eosio::contract("eosio.system")]] system_contract : public contract {
-      public:
-         using contract::contract;
-   };
+static constexpr symbol CORE_SYMBOL = symbol(symbol_code("EOS"), 4);
 
-} /// namespace eosio
+/**
+ * @defgroup system_contract eosio.system
+ * @ingroup eosiocontracts
+ *
+ * eosio.system contract
+ *
+ * @details eosio.system is the system contract for eosforce
+ * @{
+ */
+class[[eosio::contract( "eosio.system" )]] system_contract : public contract {
+   public:
+      using contract::contract;
+
+   public:
+      struct [[eosio::table]] account_info {
+         uint64_t name      = 0;
+         asset    available = asset{ 0, CORE_SYMBOL };
+
+         uint64_t primary_key() const { return name; }
+      };
+      
+      typedef eosio::multi_index< "accounts"_n, account_info > accounts_table;
+
+   public:
+      [[eosio::action]]
+      void transfer( const name& from, const name& to, const asset& quantity, const string& memo );
+};
+
+} // namespace eosio
--- contracts/eosio.system/src/eosio.system.cpp
+++ contracts/eosio.system/src/eosio.system.cpp
@@ -1,5 +1,29 @@
-#include <force.system/force.system.hpp>
+#include <utility>
+
+#include <eosio.system.hpp>
 
 namespace eosio {
+
+   void system_contract::transfer( const name& from, 
+                                   const name& to, 
+                                   const asset& quantity,
+                                   const string& memo ) {
+      require_auth( from );
+      accounts_table acnts_tbl( _self, _self.value );
+      const auto& from_act = acnts_tbl.get( from.value, "from account is not found in accounts table" );
+      const auto& to_act = acnts_tbl.get( to.value, "to account is not found in accounts table" );
+
+      check( quantity.symbol == CORE_SYMBOL, "only support EOS which has 4 precision" );
+      // from_act.available is already handling fee
+      check( 0 <= quantity.amount && quantity.amount <= from_act.available.amount,
+             "need 0.0000 EOS < quantity < available balance" );
+      check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+      require_recipient( from );
+      require_recipient( to );
+
+      acnts_tbl.modify( from_act, name{}, [&]( account_info& a ) { a.available -= quantity; } );
+      acnts_tbl.modify( to_act, name{}, [&]( account_info& a ) { a.available += quantity; } );
+   }
 
 } /// namespace eosio
