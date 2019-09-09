290749f27869618f5bee4a6dd84d4873e1899015
fanyang <fanyang.coder@gmail.com>
2019-09-09 11:31:52 +0800

    make all func typ a include header file

--- contracts/include/eosforce/assetage.hpp
+++ contracts/include/eosforce/assetage.hpp
@@ -4,6 +4,8 @@
 #include <eosio/asset.hpp>
 #include <eosio/crypto.hpp>
 
+#include <eosforce/eosforce.hpp>
+
 namespace eosforce {
    namespace utils{
       constexpr inline int64_t precision_base( const uint8_t decimals ) {
@@ -49,8 +51,6 @@ namespace eosforce {
    static constexpr eosio::name active_permission{"active"_n};
    static constexpr eosio::name block_out_pledge{"block.out"_n};
    static constexpr eosio::name pledge_account{"eosio.pledge"_n};
-
-   static constexpr auto bp_punish = "c.bppunish"_n;
 
    // asset age : asset age is a value equal asset * age by block num, in eosforce
    //             it used in many pos.
---
+++ contracts/include/eosforce/eosforce.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <eosio/eosio.hpp>
+
+namespace eosforce {
+   namespace chain_func {
+      static constexpr auto bp_punish = "c.bppunish"_n;
+   }
+} // namespace eosforce
