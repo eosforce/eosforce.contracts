f13225360ddd5e52eab3e3795e769897ab504b98
fanyang <fanyang.coder@gmail.com>
2019-09-11 18:22:26 +0800

    imp is_account_freezed by freeze table

--- contracts/eosio.system/src/freeze.cpp
+++ contracts/eosio.system/src/freeze.cpp
@@ -1,11 +1,25 @@
 #include <utility>
 
 #include <eosio.system.hpp>
+#include <eosio.freeze/eosio.freeze.hpp>
 
 namespace eosio {
 
+   static constexpr name freeze_account = "eosio.freeze"_n;
+
    bool system_contract::is_account_freezed( const account_name& account, const uint32_t curr_block_num ) {
-      return false;
+      global_freezed_state gfstate( freeze_account, freeze_account.value );
+
+      const auto act_table = gfstate.exists() ? gfstate.get().committer : 0;
+
+      if( act_table == 0 ){
+         return false;
+      }
+
+      freezeds freeze_tbl( freeze_account, act_table );
+      const auto itr = freeze_tbl.find( account );
+      return ( itr != freeze_tbl.end() ) 
+          && ( itr->unfreeze_block_num <= 1024 || curr_block_num <= itr->unfreeze_block_num );
    }
 
 } /// namespace eosio
